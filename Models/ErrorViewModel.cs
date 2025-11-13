476d2bbd27ac9809f842641552114d63b5062e76
kimihito <tatsurotamashiro@gmail.com>
2025-11-13 21:37:51 +0900

    dotnet new mvc -o AspnetTodoapp

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AspnetTodoapp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
